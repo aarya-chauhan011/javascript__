d72ecea0ee2a323444533dcb9bf48b2260de37aa
Aarya Chauhan <aaryac2004@gmail.com>
2026-02-05 16:23:49 +0000

    conversions of datatypes

---
+++ 01_basics/03_conversionsoperations.js
@@ -0,0 +1,11 @@
+let score = "33abc";
+console.log(score);
+console.log((score));
+let valueinnumber = number(score)
+console.log(typeof valueinnumber);
+console.log(valueinnumber);
+
+// in conversions
+//1 => true; 0 => false;
+// "" => false
+// "hitesh" => true
