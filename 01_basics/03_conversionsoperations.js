ea293c87fcd3492791b5d76fded984d530e54cbd
Aarya Chauhan <aaryac2004@gmail.com>
2026-02-05 17:37:52 +0000

    some comparisons

--- 01_basics/03_conversionsoperations.js
+++ 01_basics/03_conversionsoperations.js
@@ -1,11 +1,22 @@
-let score = "33abc";
+/*let score = "33abc";
 console.log(score);
 console.log((score));
 let valueinnumber = number(score)
 console.log(typeof valueinnumber);
-console.log(valueinnumber);
+console.log(valueinnumber);*/
 
 // in conversions
 //1 => true; 0 => false;
 // "" => false
 // "hitesh" => true
+
+
+//****************operations****************
+/*let value = 3;
+let negvalue = -value
+ console.log(negvalue);*/
+
+
+
+ //=== it is strict check
+ console.log("2" === 2);// yeh type or value dono check karta hai but == shirf value check karta hain
