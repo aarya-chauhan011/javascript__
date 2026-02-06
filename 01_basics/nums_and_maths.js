2207cee614ac1b0984a14b1507ca085082e169e2
Aarya Chauhan <aaryac2004@gmail.com>
2026-02-06 16:10:19 +0000

    matha and num

---
+++ 01_basics/nums_and_maths.js
@@ -0,0 +1,13 @@
+/*const score = 400
+console.log(score);
+const balance = new Number(100)
+console.log(balance);
+console.log(balance.toString());
+console.log(balance.toString().length);*/
+//jab no ek digit tk fix karne ho to .tofixed () likho
+
+//****************maths****** */
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.3))
+console.log(Math.random()*10)+1)// 0 and 1 k bich m value
