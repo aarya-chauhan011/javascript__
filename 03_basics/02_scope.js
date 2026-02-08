f56be11c800ce4f1560fe35d0c9c787fc6a27b03
Aarya Chauhan <aaryac2004@gmail.com>
2026-02-08 14:30:42 +0000

    scopes

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,24 @@
+// block and global scope
+/*let a = 100;
+if(true){
+    let a = 10;
+    const b = 20;
+    console.log("INNER: ", a);
+}
+console.log(a);*/
+
+//nested scope
+/*function one(){
+    const username = "hitesh"
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    two()
+}
+one()*/
+
+//***********************interesting ************/
+
+
+
